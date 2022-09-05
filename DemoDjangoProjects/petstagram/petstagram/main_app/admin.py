c1e7b4cd56a1690b6d23bea39ebbd4ded35dbc70
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-09-05 19:58:48 +0300

    added petstagram

--- DemoDjangoProjects/petstagram/petstagram/main_app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from petstagram.main_app.models import Profile, Pet, PetPhoto
--- DemoDjangoProjects/petstagram/petstagram/restapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from petstagram.restapp.models import Profile, Pet, PetPhoto
-
-admin.site.register(Pet)
-admin.site.register(PetPhoto)
-admin.site.register(Profile)
