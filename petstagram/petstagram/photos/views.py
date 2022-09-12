daf138e110892b7c507aca2c18a79801f142ac66
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-09-12 17:37:53 +0300

    Added edit photo page

--- petstagram/petstagram/photos/views.py
+++ petstagram/petstagram/photos/views.py
@@ -8,12 +8,16 @@ from petstagram.photos.models import Photo
 
 def add_photo(request):
     form = CreatePhotoForm(request.POST or None)
+
     if form.is_valid():
         pet = form.save(commit=False)
         pet.user_profile = request.user
         pet.save()
+
         return redirect('home')
+
     context = {"form": form}
+
     return render(request=request, template_name='photos/photo-add-page.html', context=context)
 
 
@@ -37,18 +41,23 @@ def show_photo_details(request, pk):
 
 def edit_photo(request, pk):
     photo = get_object_or_404(Photo, pk=pk)
+
     if request.method == "GET":
         form = EditPhotoForm(initial=photo.__dict__)
     else:
         form = EditPhotoForm(request.POST, instance=photo)
+
         if form.is_valid():
             form.save()
             return redirect('photo-details', pk)
+
     context = {'form': form}
+
     return render(request=request, template_name='photos/photo-edit-page.html', context=context)
 
 
 def delete_photo(request, pk):
     photo = get_object_or_404(Photo, pk=pk)
     photo.delete()
+
     return redirect('home')
