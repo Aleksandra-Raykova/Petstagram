5808842986c013633698580b6c42e3d51ec94b8b
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-09-12 17:48:26 +0300

    Added share functionality

--- petstagram/petstagram/common/urls.py
+++ petstagram/petstagram/common/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.show_home_page, name='home'),
     path('comment/<int:photo_pk>/', views.add_comment_view, name='add-comment'),
     path('like/<int:photo_pk>/', views.like_functionality, name='like'),
+    path('share/<int:photo_pk>/', views.copy_link_to_clipboard, name='share'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- petstagram/petstagram/common/views.py
+++ petstagram/petstagram/common/views.py
@@ -4,11 +4,14 @@ from petstagram.common.forms import CommentForm
 from petstagram.common.models import Like
 from petstagram.photos.models import Photo
 
+from pyperclip import copy
+
 
 def show_home_page(request):
     all_photos = Photo.objects.all()
     comment_form = CommentForm()
     context = {"all_photos": all_photos, "comment_form": comment_form}
+
     return render(request=request, template_name='common/home-page.html', context=context)
 
 
@@ -35,5 +38,14 @@ def like_functionality(request, photo_pk):
     else:
         like = Like(photo=photo, user=request.user)
         like.save()
+
+    return redirect(request.META['HTTP_REFERER'] + f'#{photo_pk}')
+
+
+def copy_link_to_clipboard(request, photo_pk):
+    if "#" in str(request.META['HTTP_REFERER']):
+        copy(request.META['HTTP_REFERER'])
+    else:
+        copy(request.META['HTTP_REFERER'] + f'#{photo_pk}')
 
     return redirect(request.META['HTTP_REFERER'] + f'#{photo_pk}')
