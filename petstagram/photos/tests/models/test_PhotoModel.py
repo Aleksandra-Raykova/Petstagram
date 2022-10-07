c6a492314c32b1d15093ffe363c32c7df1ee4e63
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-10-07 15:55:45 +0300

    Added more tests

--- petstagram/photos/tests/models/test_PhotoModel.py
+++ petstagram/photos/tests/models/test_PhotoModel.py
@@ -1,44 +1,11 @@
-from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import TestCase
 
-from petstagram.accounts.models import Profile
-from petstagram.pets.models import Pet
+from petstagram.common.mixins.SetUp_mixin import SetUpMixin
 from petstagram.photos.models import Photo
 
 
-UserModel = get_user_model()
-
-
-class PhotoModelTests(TestCase):
-    def setUp(self):
-        self.user = UserModel(
-            username='softuni-pets',
-            password='softuni-python-web',
-        )
-
-        self.user.full_clean()
-        self.user.save()
-
-        self.profile = Profile(
-            user=self.user,
-            email="softuni-petstagram@softuni.com",
-        )
-
-        self.profile.full_clean()
-        self.profile.save()
-
-        self.pet = Pet(
-            name="Axolotl",
-            pet_photo="https://i.natgeofe.com/n/de94c416-6d23-45f5-9708-e8d56289268e/naturepl_01132178.jpg?w=636&h=631",
-            user_profile=self.profile,
-            slug="Axolotl"
-        )
-
-        self.pet.full_clean()
-        self.pet.save()
-
+class PhotoModelTests(SetUpMixin):
     def test_photo_model_create__with_valid_data__expect_success(self):
         photo = Photo()
         photo.created_by_user = self.profile
@@ -72,7 +39,7 @@ class PhotoModelTests(TestCase):
 
         self.assertIsNotNone(context.exception)
 
-    def test_description_less_than10_but_more_than_0_symbols__expect_fail(self):
+    def test_description_less_than_10_but_more_than_0_symbols__expect_fail(self):
         photo = Photo()
         photo.created_by_user = self.profile
         photo.description = 'd' * 9
---
+++ petstagram/photos/tests/views/test_Add_Photo.py
@@ -0,0 +1,45 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse
+
+from petstagram.common.mixins.SetUp_mixin import SetUpMixin
+from petstagram.photos.models import Photo
+
+
+class AddPhotoViewTests(SetUpMixin):
+    def test_get__expect_correct_template(self):
+        self.client.force_login(self.user)
+
+        response = self.client.get(reverse("photo-add-page"))
+        self.assertTemplateUsed(response, 'photos/photo-add-page.html')
+
+    def test_get__expect_form_in_context(self):
+        self.client.force_login(self.user)
+
+        response = self.client.get(reverse("photo-add-page"))
+        self.assertIn('form', response.context)
+
+    def test_post__except_redirect_to_home(self):
+        self.client.force_login(self.user)
+
+        photo_file = SimpleUploadedFile(
+            name='test_image.jpg',
+            content=open('./petstagram/photos/tests/models/images/dambo.jpg', 'rb').read(),
+            content_type='image/jpeg'
+        )
+
+        response = self.client.post(reverse("photo-add-page"), {'photo_file': photo_file})
+        self.assertRedirects(response, reverse("home"))
+
+    def test_post__expect_photo_model_save(self):
+        self.client.force_login(self.user)
+
+        photos_len = len(Photo.objects.all())
+
+        photo_file = SimpleUploadedFile(
+            name='test_image.jpg',
+            content=open('./petstagram/photos/tests/models/images/dambo.jpg', 'rb').read(),
+            content_type='image/jpeg'
+        )
+
+        self.client.post(reverse("photo-add-page"), {'photo_file': photo_file})
+        self.assertEqual(photos_len + 1, len(Photo.objects.all()))
